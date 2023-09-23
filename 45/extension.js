7a6ad51ee6e53d258025234c38f27f5ee34113d8
TheBrokenRail <connor24nolan@live.com>
2023-09-23 15:24:13 -0400

    Add GNOME 45 Support

---
+++ 45/extension.js
@@ -0,0 +1,23 @@
+import * as KeyboardUI from 'resource:///org/gnome/shell/ui/keyboard.js';
+
+function _modifiedLastDeviceIsTouchscreen() {
+    return false;
+}
+
+export default class BlockCaribou {
+    constructor() {
+        this._originalLastDeviceIsTouchscreen = null;
+    }
+
+    enable() {
+        this._originalLastDeviceIsTouchscreen = KeyboardUI.KeyboardManager.prototype._lastDeviceIsTouchscreen;
+        KeyboardUI.KeyboardManager.prototype._lastDeviceIsTouchscreen = _modifiedLastDeviceIsTouchscreen;
+    }
+
+    disable() {
+        if (this._originalLastDeviceIsTouchscreen !== null) {
+            KeyboardUI.KeyboardManager.prototype._lastDeviceIsTouchscreen = this._originalLastDeviceIsTouchscreen;
+            _originalLastDeviceIsTouchscreen = null;
+        }
+    }
+}
